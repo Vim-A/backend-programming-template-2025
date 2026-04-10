0cca73015579776be6cba14c3d75b6f61d28b7d3
Vim-A <ropildavid@gmail.com>
2026-04-11 03:08:41 +0700

    gacha controller fix

--- src/api/components/gacha/gacha-controller.js
+++ src/api/components/gacha/gacha-controller.js
@@ -1,5 +1,6 @@
 const gachaService = require('./gacha-service');
 const { errorResponder, errorTypes } = require('../../../core/errors');
+const { ne } = require('@faker-js/faker');
 
 async function Ngegacha(request, response, next) {
   try {
@@ -18,6 +19,42 @@ async function Ngegacha(request, response, next) {
     });
 
     return response.status(200).json(result);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function Gethistorigacha(request, response, next) {
+  try {
+    const { user_id: userId } = request.body;
+    if (!userId) {
+      throw errorResponder(
+        errorTypes.VALIDATION_EROR, ' user id di isi ya');
+    }
+    const Histori = await gachaService.Historigacha({
+      userId,
+    });
+    return response.status(200).json(Histori);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function GetlistHadiah(request, response, next) {
+  try {
+    const hadiah = await gachaService.DptHadiah();
+
+    return response.status(200).json(hadiah);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function getListPemenang(request, response, next) {
+  try{
+    const pemenang = await gachaService.getListPemenang();
+
+    return response.status(200).json(pemenang);
   } catch (error) {
     return next(error);
   }
